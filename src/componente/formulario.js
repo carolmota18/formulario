41842215e64c5a6d409a20996faf29f3d098a5b2
vai na web <vainawebcpx@gmail.com>
2019-12-04 12:28:41 -0300

    subindo projeto

--- src/componente/formulario.js
+++ src/componente/formulario.js
@@ -1,28 +1,116 @@
 import React from 'react';
 
-class Formulario extends React.Component {
-        // constructor(props) {
-        //     super(props)
-        // }
-
-    mudarNome = (ev) => {
-        console.log(ev)
+class Lista extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            listadealunos: [
+                    {
+                        nome:'caroline',
+                         idade:'19',
+                        polo:'pzr'
+                    },
+                    {
+                        nome:'geysa',
+                        idade:'20',
+                        polo:'pzr',
+                    },
+                    {
+                        nome:'cleiton',
+                        idade:'19',
+                        polo:'cpx',
+                    },
+                    {
+                        nome:'maicon',
+                        idade:'23',
+                        polo:'cpx',
+                    },
+                    {
+                        nome:'estefania',
+                        idade:'22',
+                        polo:'pzr'
+                    },
+                    {
+                        nome:'jorge',
+                        idede:'58',
+                        polo:'cpx'
+                
+                    },
+                    {
+                        nome:'rhuan',
+                        idade:'20',
+                        polo:'pzr'
+                    },
+                    {
+                        nome:'dandara',
+                        idade:'21',
+                        polo:'cpx'
+                    },
+                    {
+                        nome:'cassio',
+                        idade:'21',
+                        polo:'prz'
+                    },
+                    {
+                        nome:'cicero',
+                        idade:'24',
+                        polo:'pzr'
+                
+                    },
+                    {
+                        nome:'luan',
+                        idade:'25',
+                        polo:'cpx'
+                    },
+                     {
+                        nome:'arthur',
+                        idade:'24',
+                        polo:'cpx'
+                     } ,
+                     {
+                         nome:'vitor',
+                         idade:'24',
+                         polo:'cpx'
+                     },
+                     {
+                        nome:'zaira',
+                        idade:'19',
+                        polo:'cpz'
+                     },
+                     {
+                         nome:'lucas',
+                         idade:'24',
+                         polo:'cpx'
+                     },
+                     {
+                         nome:'renan',
+                         idade:'14',
+                         polo:'pzr'
+                     },
+                     {
+                         nome:'douglas',
+                         idade:'24',
+                         polo:'cpz'
+                     },
+                     {
+                         nome:'priscila',
+                         idade:'32',
+                         polo:'pzr'
+                     }
+        ]   
+        }
+    }
+    mostrarusuarios = () => {
+        console.log('carol')
+        this.state.listadealunos.map(callback=>{
+            console.log(callback.nome)
+        })
     }
 
-    render(){
-        return(
-            <div>
-                    nome:
-                <input onChange={this.mudarNome}>
-                </input>
-                    idade:
-                <input>
-                </input>
-                    cidade:
-                <input>
-                </input>
-            </div>
+    render() {
+        return (
+            <button onClick={this.mostrarusuarios}>mostrar alunos</button>
         )
     }
 }
-export default Formulario;
+export default Lista;
